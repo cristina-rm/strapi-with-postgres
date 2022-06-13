0f10887605250b301de234dd060a7a74b851cf0c
Cristina <cristina@ucan.be>
2022-06-13 10:27:41 +0200

    Update database config

--- config/database.js
+++ config/database.js
@@ -1,4 +1,41 @@
-module.exports = ({ env }) => ({
+const path = require('path');
+
+module.exports = ({ env }) => {
+  if (env('NODE_ENV') === 'production') {
+    return {
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: env('DATABASE_HOST', '127.0.0.1'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'dbname'),
+          user: env('DATABASE_USERNAME', 'db_user'),
+          password: env('DATABASE_PASSWORD', 'password'),
+          ssl: {
+            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+          },
+        },
+        debug: false,
+      },
+    };
+  }
+
+  return {
+    connection: {
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'dbname'),
+        user: env('DATABASE_USERNAME', 'db_user'),
+        password: env('DATABASE_PASSWORD', 'password'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+    },
+  }
+};
+
+/*module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
@@ -10,4 +47,4 @@ module.exports = ({ env }) => ({
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
-});
+});*/
